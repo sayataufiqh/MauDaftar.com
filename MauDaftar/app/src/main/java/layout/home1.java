1c13230865a2371958db0b45e46afdbe3867913c
DESKTOP-RURIL34\Saya <taufiq@null.net>
2016-12-19 16:11:40 +0800

    coba

---
+++ MauDaftar/app/src/main/java/layout/home1.java
@@ -0,0 +1,24 @@
+package layout;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yandi.maudaftar.R;
+
+
+public class home1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home1, container, false);
+    }
+
+}
